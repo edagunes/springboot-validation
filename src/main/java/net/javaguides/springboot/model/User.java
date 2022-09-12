a0d1f6a9444945925bbcccd2f7b859c20192c83f
Eda Güneş <gunnes.eda@gmail.com>
2022-09-12 10:49:38 +0300

    first commit

---
+++ src/main/java/net/javaguides/springboot/model/User.java
@@ -0,0 +1,60 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+
+@Table(name="users")
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Pattern(regexp = "^[0-9]{10}$")
+	private String taxno;
+	
+	@NotEmpty
+	@Email
+	private String email;
+	
+	public User() {
+		
+	}
+	
+	public User(String taxno, String email) {
+		super();
+		this.taxno = taxno;
+		this.email = email;
+	}
+
+
+	public String getTaxno() {
+		return taxno;
+	}
+	public void setTaxno(String taxno) {
+		this.taxno = taxno;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
